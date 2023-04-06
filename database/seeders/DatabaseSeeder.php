57667ff8c41a06812c4aa527c7f726e11480fd5e
“ecaf” <shiyaan@gmail.com>
2023-04-06 20:49:57 +0500

    testing auto deploy

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $user = new User;
+        $user->email = "employer@test.com";
+        $user->name = "Employer 1";
+        $user->password = 'welcome';
+        $user->user_type = 'employer';
+        $user->save();
     }
 }
