75c24ce1dda608139c95a314cc0ac293e89b4f23
“ecaf” <shiyaan@gmail.com>
2023-04-06 20:34:31 +0500

    testing auto deploy

--- resources/js/components/pages/Login.js
+++ resources/js/components/pages/Login.js
@@ -73,7 +73,7 @@ export const Login = (props) => {
         <div className="row justify-content-center">
             <div className="col-md-8">
                 <div className="card">
-                    <div className="card-header">EMP Login</div>
+                    <div className="card-header"> Login</div>
 
                     <div className="card-body">
 
